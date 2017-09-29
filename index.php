fbb398c46938c9268cca4cc6e4d8b55b6a24a81d
Volunter <homerjsimpsons1998@gmail.com>
2017-09-29 13:36:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -37,29 +37,21 @@ name="email" id="email"/></br>
 name="submit" value="Submit" />
 </form>
 <?php
-// DB connection info
-$host = "localhost\sqlexpress";
-$user =  "vol1";
-$pwd = "Simpsons1";
-$db = "NewBD";
-try{
-    $conn = new PDO
-( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    $conn->setAttribute
-( PDO::ATTR_ERRMODE, 
-PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE registration_tbl(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-catch(Exception $e){
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-echo "<h3>Table created.</h3>";
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "vol1@vol1", "pwd" => "Simpsons1", "Database" => "NewBD", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:vol1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 ?>
 </body>
 </html>
