1942aea2a5d1bacbe593be0e437c6cdfeb1a85b2
Volunter <homerjsimpsons1998@gmail.com>
2017-10-03 15:46:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -39,13 +39,7 @@ name="submit" value="Submit" />
     try {
     $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE registrationf_tbl2(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
+   
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
