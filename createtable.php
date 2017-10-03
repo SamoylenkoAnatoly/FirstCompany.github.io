e3dd691946ff97f1683ef344cc51bc8f120aa854
Volunter <homerjsimpsons1998@gmail.com>
2017-10-03 14:57:15 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,6 +1,14 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
