778cf1947f471f43f610f02e739bf06fed1fd2b4
Volunter <homerjsimpsons1998@gmail.com>
2017-10-05 13:39:57 +0300

    Update and rename registration to registration.php

---
+++ registration.php
@@ -0,0 +1,10 @@
+
+<form method="post" action="index.php" 
+enctype="multipart/form-data" >
+      Name  <input type="text" 
+name="name" id="name"/></br>
+      Email <input type="text" 
+name="email" id="email"/></br>
+      <input type="submit" 
+name="submit" value="Submit" />
+</form>
