a533464a26fbf7e21672aecb94d5980b8a70c4d5
Volunter <homerjsimpsons1998@gmail.com>
2017-11-21 10:00:11 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -5,12 +5,9 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 	
-	$error = array(); //массив для ошибок 	
-if ($_POST['Login'] != "" && $_POST['Password'] != "") //если поля заполнены 	
-
-{ 		
+		
 	print ("hello");
-} else {print ("123")}
+
 	
 }
 catch (PDOException $e) {
