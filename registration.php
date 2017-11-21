cef0b0b9303781007ab00e8b9a0f634fd5dc57e3
Volunter <homerjsimpsons1998@gmail.com>
2017-11-21 09:58:10 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,13 +1,5 @@
 
 
-<form method="post" action="registration.php"
-enctype="multipart/form-data" >
-      Login  <input type="text" 
-name="Login" id="Login"/></br>
-      Password <input type="text" 
-name="Password" id="Password"/></br>
-      <input type="submit" 
-name="Submit1" value="Submit1" />
 <?php
 try {
     $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
