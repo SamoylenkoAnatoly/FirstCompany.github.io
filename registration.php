d94aa1db7b5277118294dab0decdeecd113d688e
Volunter <homerjsimpsons1998@gmail.com>
2017-11-21 09:52:33 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -19,40 +19,11 @@ try {
 if ($_POST['Login'] != "" && $_POST['Password'] != "") //если поля заполнены 	
 
 { 		
-	$login = $_POST['Login']; 
-	$password = $_POST['Password'];
-
-	$rez = mysql_query("SELECT * FROM registration_tbl WHERE name=$login"); //запрашиваем строку из БД с логином, введённым пользователем 		
-	if (mysql_num_rows($rez) == 1) //если нашлась одна строка, значит такой юзер существует в БД 		
-
-	{ 			
-		$row = mysql_fetch_assoc($rez); 			
-		if (md5(md5($password).$row['salt']) == $row['Password']) //сравниваем хэшированный пароль из БД с хэшированными паролем, введённым пользователем и солью (алгоритм хэширования описан в предыдущей статье) 						
-
-		{ 
-		//пишем логин и хэшированный пароль в cookie, также создаём переменную сессии
-		setcookie ("name", $row['name'], time() + 50000); 						
-		setcookie ("email", md5($row['name'].$row['email']), time() + 50000); 					
-		$_SESSION['id'] = $row['id'];	//записываем в сессию id пользователя 				
-
-		$id = $_SESSION['id']; 				
-		lastAct($id); 				
-		return $error; 			
-	} 			
-	else //если пароли не совпали 			
-
-	{ 				
-		$error[] = "Неверный пароль"; 										
-		return $error; 			
-	} 		
+	print "hello";
+} 
+else{ 
+	
 } 		
-	else //если такого пользователя не найдено в БД 		
-
-	{ 			
-		$error[] = "Неверный логин и пароль"; 			
-		return $error; 		
-	} 	
-} 	
 	else 	
 	{ 		
 		$error[] = "Поля не должны быть пустыми!"; 				
