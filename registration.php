8509de6a112bea965f3daa18e69df871f0e8b47f
Volunter <homerjsimpsons1998@gmail.com>
2017-11-24 14:17:47 +0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,13 +1,15 @@
 
-
 <?php
 try {
     $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 	
-$a = "Select name From registration_tbl";
+$a = "Select name From registration_tbl where id = ";
 $b = "Select email From registration_tbl";
-	$conn->print query($a);
+
+	for(int i = 0 ; i<){
+	
+	}
 	
 		
 	
