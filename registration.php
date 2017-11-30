65bd08292e84dd064b886dcc98b9cf3281c08a4e
Volunter <homerjsimpsons1998@gmail.com>
2017-11-30 13:46:51 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -25,7 +25,22 @@ try {
       $sql_select = "SELECT * FROM registration_tbl where name= abc And email = abc";
       $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll(); 
-      
+      if(count($registrants) > 0) {
+    echo "<h2>People who are registered:</h2>";
+    echo "<table>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Date</th></tr>";
+    foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+        echo "<td>".$registrant['date']."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h3>No one is currently registered.</h3>";
+}
+
 
       
       
