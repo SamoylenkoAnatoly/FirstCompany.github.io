151c9667e071d672df65310dd0e9a06d28d313e5
Volunter <homerjsimpsons1998@gmail.com>
2017-11-30 13:39:34 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,4 +1,22 @@
-
+<html>
+<head>
+<Title>Registration Form</Title>
+</head>
+<body>
+<h1>Register here!</h1>
+<p>Fill in your name and 
+email address, then click <strong>Submit</strong> 
+to register.</p>
+<form method="post" action="index.php" 
+enctype="multipart/form-data" >
+      Name  <input type="text" 
+name="name" id="name"/></br>
+      Email <input type="text" 
+name="email" id="email"/></br>
+      <input type="submit" 
+name="submit" value="Submit" />
+       <input type="submit" 
+name="submit2" value="Очистка" />
 <?php
 try {
     $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
@@ -43,3 +61,7 @@ catch (PDOException $e) {
 }
 
 ?>
+</form>
+
+</body>
+</html>
