548b897382f8977a1282506333f01f1ed994705c
Volunter <homerjsimpsons1998@gmail.com>
2017-11-30 13:48:21 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -22,7 +22,7 @@ name="submit2" value="Очистка" />
 try {
     $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-      $sql_select = "SELECT * FROM registration_tbl where name = abc And email = abc";
+      $sql_select = "SELECT * FROM registration_tbl where name = 'abc' And email = 'abc'";
       $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll(); 
       if(count($registrants) > 0) {
