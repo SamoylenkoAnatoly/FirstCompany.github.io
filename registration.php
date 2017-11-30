1f83baa33da0ac5f3b5564b06734a858a4de2df4
Volunter <homerjsimpsons1998@gmail.com>
2017-11-30 13:45:11 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -22,6 +22,7 @@ name="submit2" value="Очистка" />
 try {
     $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+      $sql_select = "SELECT * FROM registration_tbl where name= abc And email = abc";
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
