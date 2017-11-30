903901144e4f710376b7bcdec1fb6075802a6a75
Volunter <homerjsimpsons1998@gmail.com>
2017-11-30 13:43:40 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -18,6 +18,18 @@ name="submit" value="Submit" />
        <input type="submit" 
 name="submit2" value="Очистка" />
 
+      <?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = NewBD", "vol1", "Simpsons1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+?>
+
 </form>
 
 </body>
