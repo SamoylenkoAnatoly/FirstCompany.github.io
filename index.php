9ea244378ae7ce8f3f0ce8c7a452da7b43ee9fb1
Volunter <homerjsimpsons1998@gmail.com>
2017-12-18 20:49:50 +0400

    Update index.php

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ catch (PDOException $e) {
       //Удаление по логину
    if(isset($_POST["submit3"]))
    {
-         if($_POST["delL"]=='')echo "Введите логин!!!";
+         
          else{
          $loginfordel = $_POST["delL"];
      $sql1 = "DELETE FROM registration_tbl Where name = '$loginfordel'";
