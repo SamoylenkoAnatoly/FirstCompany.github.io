2639efe5fa418e06e0ed9520c10b509280da5120
Volunter <homerjsimpsons1998@gmail.com>
2017-12-18 21:02:56 +0400

    Update index.php

--- index.php
+++ index.php
@@ -106,8 +106,9 @@ catch (PDOException $e) {
          $loginfordel = $_POST["delL"];
      $sql1 = "DELETE FROM registration_tbl Where name = '$loginfordel'";
     $conn->query($sql1);
-         echo "Удаление произошло";}
-          else{echo "Данный пользователь не найден";}
+      ;
+               echo "Данный пользователь не найден";}
+          else{   echo "Удаление произошло"}
          
          }
    } 
